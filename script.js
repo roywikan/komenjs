4216e22dc6dd17f135ea2b67c1342af3c5c70215
wee <5243434+roywikan@users.noreply.github.com>
2023-11-21 20:40:25 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+function submitComment() {
+    const comment = document.getElementById('comment').value;
+
+    // Use your GitHub username, repository name, and personal access token
+    const owner = 'your-username';
+    const repo = 'your-repo-name';
+    const path = 'comments.txt';
+    const token = 'your-personal-access-token';
+
+    // Encode the comment content
+    const content = btoa(comment);
+
+    // Make the API request
+    fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`, {
+        method: 'PUT',
+        headers: {
+            'Authorization': `token ${token}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            message: 'Add new comment',
+            content: content,
+        }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Comment submitted successfully:', data);
+        // You can handle success here (e.g., update the UI).
+    })
+    .catch(error => console.error('Error submitting comment:', error));
+}
